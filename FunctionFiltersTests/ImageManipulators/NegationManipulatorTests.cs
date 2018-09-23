dd40dd3425ebcf091985d181ac557553113b2372
monkog <monkog@wp.pl>
2018-09-23 22:21:58 +0200

    Negation Manipulator Tests

---
+++ FunctionFiltersTests/ImageManipulators/NegationManipulatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using FunctionFilters.ImageManipulators;
+using Xunit;
+
+namespace FunctionFiltersTests.ImageManipulators
+{
+	public class NegationManipulatorTests
+	{
+		[Fact]
+		public void Negate_White_Black()
+		{
+			var black = Color.Black;
+			var result = Color.White.Negate();
+
+			Assert.Equal(black.A, result.A);
+			Assert.Equal(black.R, result.R);
+			Assert.Equal(black.G, result.G);
+			Assert.Equal(black.B, result.B);
+		}
+
+		[Fact]
+		public void Negate_Black_White()
+		{
+			var white = Color.White;
+			var result = Color.Black.Negate();
+
+			Assert.Equal(white.A, result.A);
+			Assert.Equal(white.R, result.R);
+			Assert.Equal(white.G, result.G);
+			Assert.Equal(white.B, result.B);
+		}
+
+		[Fact]
+		public void Negate_Color_Negated()
+		{
+			var color = Color.FromArgb(15, 30, 55);
+			var result = color.Negate();
+
+			Assert.Equal(color.A, result.A);
+			Assert.Equal(240, result.R);
+			Assert.Equal(225, result.G);
+			Assert.Equal(200, result.B);
+		}
+	}
+}
---
+++ FunctionFiltersTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FunctionFiltersTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FunctionFiltersTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("817538eb-d632-42b4-9b32-3b43d517e575")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
